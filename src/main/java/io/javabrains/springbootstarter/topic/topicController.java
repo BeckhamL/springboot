2149ad627a7c3e272da65c4a1e934f7d7038456d
beckham <beckham.lam@mail.mcgill.ca>
2018-10-13 21:28:18 -0400

    topic REST controller

---
+++ src/main/java/io/javabrains/springbootstarter/topic/topicController.java
@@ -0,0 +1,18 @@
+package io.javabrains.springbootstarter.topic;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class topicController {
+
+    // Gets converted to JSON automatically
+    @RequestMapping("/topic")
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+                new Topic("beckham", "5134", "sure"),
+                new Topic("ok", "sda", "sada")
+        );
+    }
+}
