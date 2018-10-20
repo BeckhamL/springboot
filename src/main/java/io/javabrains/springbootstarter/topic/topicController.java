d293152479368995b8211a87a5e37154621ac959
beckham <beckham.lam@mail.mcgill.ca>
2018-10-20 19:29:02 -0400

    POST and DELETE request

--- src/main/java/io/javabrains/springbootstarter/topic/topicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/topicController.java
@@ -11,18 +11,33 @@ public class topicController extends Topic{
     private TopicService topicService;
 
     // Gets converted to JSON automatically
+    // Get all topics
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
         return topicService.getAllTopics();
     }
 
+    // Get a certain topic
     @RequestMapping("/topics/{id}")
     public Topic getTopic(@PathVariable String id) {
         return topicService.getTopic(id);
     }
 
+    // Add a topic
     @RequestMapping(method = RequestMethod.POST, value = "/topics")
     public void addTopic(@RequestBody Topic topic) {
         topicService.addTopic(topic);
+    }
+
+    // Update topics
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.updateTopic(topic, id);
+    }
+
+    // Delete a certain topic
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id) {
+        topicService.deleteTopic(id);
     }
 }
